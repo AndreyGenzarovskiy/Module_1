1484ec9b3b89e3e6c731e491e7b4a56918cdd8d8
AndreyGenzarovskiy <andrey.genzarovskiy@gmail.com>
2022-07-12 13:53:50 +0300

    78% lines covered

--- Level_2/src/test/java/task_1/ValidationBracketsTest.java
+++ Level_2/src/test/java/task_1/ValidationBracketsTest.java
@@ -8,9 +8,17 @@ class ValidationBracketsTest {
 
     @Test
     void isValidBrackets() {
-        String Str = "Sdjjllf(){}[]sdkjk()";
+        String str = "Sdjjllf(){}[]sdkjk()";
         boolean expected = true;
-        boolean actual = ValidationBrackets.isValidBrackets("Sdjjllf(){}[]sdkjk()");
+        boolean actual = ValidationBrackets.isValidBrackets(str);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isValidBracketsNegative() {
+        String str = "Sdjjllf(){]sdkjk()";
+        boolean expected = false;
+        boolean actual = ValidationBrackets.isValidBrackets(str);
         assertEquals(expected, actual);
     }
 }
